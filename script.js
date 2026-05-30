e39f39995fefabaf7b37f8715f0d0cd1704ee1f8
0xsadik <torikussadik404@gmail.com>
2026-05-30 23:11:52 +0600

    navbar done

---
+++ script.js
@@ -0,0 +1,17 @@
+
+// -- navigation ---
+const navToggle = document.getElementById('navToggle');
+const navMenu = document.getElementById('navMenu');
+const navLinks = document.querySelectorAll('.nav-link');
+
+navToggle.addEventListener('click', () => {
+    navToggle.classList.toggle('active');
+    navMenu.classList.toggle('active');
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        navToggle.classList.remove('active');
+        navMenu.classList.remove('active');
+    });
+});
